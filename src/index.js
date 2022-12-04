bf598333a1334be03bd3b661ede62394ad7103f3
sava642 <101993338+sava642@users.noreply.github.com>
2022-12-04 19:05:25 +0100

    main

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
 
   <Provider store={store}>
     <PersistGate loading={null} persistor={persistor}>
-      <BrowserRouter>
+      <BrowserRouter basename='/redux-registration/'>
         <App />
       </BrowserRouter>
     </PersistGate>
